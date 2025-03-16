faeaba296f70dc9640612c6266569d3cfad9ff91
Vivek <vivekgupta7a@gmail.com>
2025-03-16 16:48:55 +0530

    change in template

--- SkillEdge-root/Community/forms.py
+++ SkillEdge-root/Community/forms.py
@@ -4,11 +4,10 @@ from .models import Post, Comment, Topic
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'topic']
+        fields = ['title', 'content']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'}),
             'content': forms.Textarea(attrs={'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500', 'rows': 5}),
-            'topic': forms.Select(attrs={'class': 'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500'}),
         }
 
 class CommentForm(forms.ModelForm):
